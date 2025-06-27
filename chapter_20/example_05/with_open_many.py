032a6e7bac463137b9d2460a8ae34ab94a2ab3bd
Eugene Ilin <jenyay.ilin@gmail.com>
2025-06-27 10:07:36 +0300

    Fix an example

--- chapter_20/example_05/with_open_many.py
+++ chapter_20/example_05/with_open_many.py
@@ -1,4 +1,4 @@
-with (open("hello.txt", "w", encoding="utf-8") as file_1,
-      open("привет.txt", "w", encoding="utf-8") as file_2):
+with (open("hello_en.txt", "w", encoding="utf-8") as file_1,
+      open("hello_ru.txt", "w", encoding="utf-8") as file_2):
     file_1.write("Hello, world!")
     file_2.write("Привет, мир!")
